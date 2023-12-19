653f03c6947505b97cf360e61481709b12107fbc
TuZaaaaa <2677824148wjt@gmail.com>
2023-12-19 08:10:18 +0800

    feature: 登录功能

---
+++ lib/share.py
@@ -0,0 +1,5 @@
+class SI:
+    def __init__(self):
+        self.login_page = None
+        self.main_page = None
+        self.page3 = None
---
+++ login.py
@@ -0,0 +1,37 @@
+import sys
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
+
+from db.sql_helper import SqlHelper
+from lib.share import SI
+from main import Main
+
+
+class Login:
+
+    def __init__(self):
+        self.ui = uic.loadUi('./ui/login.ui')
+        self.sql_helper = SqlHelper()
+
+        # 登录
+        self.ui.pushButton_login.clicked.connect(self.login)
+
+    def login(self):
+        print('login')
+        name = self.ui.lineEdit_username.text()
+        password = self.ui.lineEdit_password.text()
+        result = self.sql_helper.get_list('select * from user where name = %s and password = %s', [name, password])
+        if len(result) == 0:
+            QMessageBox.warning(self.ui, '登录失败', '用户名或密码错误', QMessageBox.Yes)
+        else:
+            SI.main_page = Main()
+            SI.main_page.ui.show()
+            self.ui.hide()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    SI.page1 = Login()
+    SI.page1.ui.show()
+    app.exec()
